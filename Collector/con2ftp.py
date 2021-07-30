284e6a22035f9321b8c61d41e486d55a1ff478d2
guianderson <guilherme.anderson34@outlook.com>
2021-07-30 19:10:03 -0300

    connect to ftp server and listing files

---
+++ Collector/con2ftp.py
@@ -0,0 +1,38 @@
+from ftplib import FTP
+
+
+class Con2ftp:
+    def __init__(self, address, folder):
+        self.address = address
+        self.folder = folder
+        self.conexao = ''
+        self.files_list = []
+
+    def conn(self):
+        self.conexao = FTP(self.address)
+        try:
+            self.conexao.login('queimadas', 'inpe_2012')
+            return ">>> " + self.conexao.getwelcome()
+        except:
+            return ' > Connection has been failed'
+
+    def countFiles(self):
+        self.conexao.cwd(self.folder)
+        log = []
+
+        nonpassive = False
+
+        self.conexao.retrlines('LIST', callback=log.append)
+        dirs = (line.rsplit(None, 1)[1] for line in log)
+
+        if nonpassive:
+            self.conexao.set_pasv(False)
+
+        for file in dirs:
+            self.files_list.append(file)
+        del (self.files_list[0:2])
+
+        return '>>> ' + str(len(self.files_list)) + ' Files found'
+
+    def listFiles(self, files):
+        return self.files_list
---
+++ Collector/main.py
@@ -0,0 +1,12 @@
+# Importar do arquivo con2ftp a classe Con2ftp
+from con2ftp import Con2ftp
+
+if __name__ == '__main__':
+
+    print('Connecting to FTP server...')
+    conn = Con2ftp('ftp.dgi.inpe.br', '/terrama2q/TerraMA2Q_408')
+    print(conn.conn())
+
+    print('Searching for burn files...')
+    print(conn.countFiles())
+    print(conn.listFiles(''))
